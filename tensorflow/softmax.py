2a85fd6038364016e7a6a78ed3f3d70b203833c6
xhb <xhb@163.com>
2017-12-06 00:52:42 +0800

    standard_scale加入标准规范化

--- tensorflow/softmax.py
+++ tensorflow/softmax.py
@@ -27,7 +27,12 @@ def xavier_init(fan_in, fan_out, constant = 1):
 								minval = low, maxval = high,
 								dtype = tf.float32)
 
-								
+def standard_scale(X_train, X_test):
+	preprocessor = prep.StandardScaler().fit(X_train)
+	X_train = preprocessor.transform(X_train)
+	X_test = preprocessor.transform(X_test)
+	return X_train, X_test
+	
 sess = tf.InteractiveSession()
 X = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
@@ -38,8 +43,8 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(X, W) + b)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-#train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
 
 correct_predict = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
@@ -49,12 +54,13 @@ tf.global_variables_initializer().run()
 for i in range(epochs):
 	batch_xs, batch_ys = mnist.train.next_batch(100)
 	val_xs, val_ys = mnist.validation.next_batch(100)
+	batch_xs, val_xs = standard_scale(batch_xs, val_xs)
 	_, acc = sess.run([train_step,accuracy], feed_dict = {X: batch_xs, y_: batch_ys})
 	val_acc = accuracy.eval({X: val_xs, y_: val_ys})
 	if i % 100 == 0:
 		print ("{}/{} train:{} val:{}".format(i, epochs, acc, val_acc))
 
-
+test_x, _ = standard_scale(test_x, val_xs)
 test_accuracy = accuracy.eval({X: test_x, y_: test_y})
 
 print ("=====================================")
