300bae90dcedcaac84f9fe09de8e558a26044c05
xhb <xhb@163.com>
2017-12-05 21:42:22 +0800

    Softmax分类

--- tensorflow/softmax.py
+++ tensorflow/softmax.py
@@ -1,10 +1,12 @@
 #-*-coding:cp936-*-
 
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
-
+import sklearn.preprocessing as prep
 
 file_data = "MNIST_data/"
+epochs = 1000
 mnist= input_data.read_data_sets(file_data, one_hot=True)
 
 train_x = mnist.train.images
@@ -17,5 +19,49 @@ val_y = mnist.validation.labels
 print ("Shape train_x:{} train_y:{}".format(train_x.shape, train_y.shape))
 print ("Shape test_x:{} test_y:{}".format(test_x.shape, test_y.shape))
 print ("Shape val_x:{} val_y:{}".format(val_x.shape, val_x.shape))
+
+def xavier_init(fan_in, fan_out, constant = 1):
+	low = -constant * np.sqrt(6.0/ (fan_in + fan_out))
+	high = constant * np.sqrt(6.0/ (fan_in + fan_out))
+	return tf.random_uniform((fan_in, fan_out), 
+								minval = low, maxval = high,
+								dtype = tf.float32)
+
+								
+sess = tf.InteractiveSession()
+X = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+#W = tf.Variable(tf.zeros([784, 10]))
+W = tf.Variable(xavier_init(784, 10))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(X, W) + b)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+#train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+
+correct_predict = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
+
+tf.global_variables_initializer().run()
+
+for i in range(epochs):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	val_xs, val_ys = mnist.validation.next_batch(100)
+	_, acc = sess.run([train_step,accuracy], feed_dict = {X: batch_xs, y_: batch_ys})
+	val_acc = accuracy.eval({X: val_xs, y_: val_ys})
+	if i % 100 == 0:
+		print ("{}/{} train:{} val:{}".format(i, epochs, acc, val_acc))
+
+
+test_accuracy = accuracy.eval({X: test_x, y_: test_y})
+
+print ("=====================================")
+print ("test acc:{}".format(test_accuracy))
+
+sess.close()
+
+
 
 
