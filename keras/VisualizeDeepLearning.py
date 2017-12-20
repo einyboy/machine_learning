a058fd1586a2b4fbd242bc664d03d6125811642f
xhb <xhb@163.com>
2017-12-20 11:31:06 +0800

    CNN简单图像分类

---
+++ keras/VisualizeDeepLearning.py
@@ -0,0 +1,10 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils.vis_utils import plot_model
+
+model = Sequential()
+model.add(Dense(2, input_dim=1, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+print(model.summary())
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
